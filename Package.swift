e59aea472bad9a5c90b8ec7d87eb87bb336d894d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-03-29 10:41:07 +0000

    [Package] lowercase dependencies

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "LinkedList", targets: ["ListEntry"])
     ],
     dependencies: [
-        .package(path: "../Test")
+        .package(path: "../test")
     ],
     targets: [
         .target(name: "ListEntry"),
